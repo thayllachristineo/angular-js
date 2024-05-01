c27c4d997c2f299a4313cbe485761a8d006a1bb5
thaylla <thayllachristineo@gmail.com>
2024-05-01 12:27:35 -0300

    structural directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
+  imports: [CommonModule],
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Bem-vindo ao treinamento de Angular';
-  imgUrl =
-    'https://miro.medium.com/v2/resize:fit:720/format:webp/1*DmU07T30U-l0aKYnGJQf-Q.png';
+  title = 'Olá';
 
-  /**
-   * 4 tipos de data bindings
-   * Interpolação: exibição de dados {{}}
-   * Property bind: <img [src]="imgUrl" />
-   * Event bind: <button (click)="onClick()">Clique aqui</button>
-   * Two-way databind: <input [(ngModel)]="title" />
-   */
-
-  constructor() {
-    // Construtor
-    console.log('Construtor');
-  }
+  constructor() {}
 }
