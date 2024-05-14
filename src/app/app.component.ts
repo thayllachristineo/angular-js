acd7a20aa08f8d9838e2b03d6a44a02648d61ff9
thaylla <thayllachristineo@gmail.com>
2024-05-14 14:31:10 -0300

    integrating angular material into the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { NewComponent } from './components/new/new.component';
 import { HomeComponent } from './components/home/home.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, MatToolbarModule, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css',
 })
-export class ToolbarComponent {}
+export class ToolbarComponent {
+  title = 'Projeto 1';
+}
