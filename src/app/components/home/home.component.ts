ec8f57bb1a424230920b53eba5ac4c45e9c0395e
thaylla <thayllachristineo@gmail.com>
2024-05-14 14:51:18 -0300

    updating table component with angular material

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,19 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
 import { GeneroPipe } from '../../pipes/genero.pipe';
+
+export interface Pessoa {
+  nome: string;
+  genero: string;
+  idade: number;
+  salario: string;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, GeneroPipe],
+  imports: [CommonModule, GeneroPipe, MatTableModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  pessoas = [
+  pessoas: Pessoa[] = [
     { nome: 'Thaylla', genero: 'F', idade: 31, salario: '1000' },
     { nome: 'Cesar', genero: 'M', idade: 26, salario: '2000' },
     { nome: 'Giovanna', genero: 'F', idade: 29, salario: '3000' },
     { nome: 'Marinalva', genero: 'F', idade: 68, salario: '4000' },
   ];
+
+  displayedColumns: string[] = ['Nome', 'Gênero', 'Idade', 'Salário'];
+  dataSource = new MatTableDataSource(this.pessoas);
 }
